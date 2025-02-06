a245dc102badd55d4cf22ab4da0e0f266122f156
shidai287 <shidai287@gotvdsb.ca>
2025-02-05 20:18:25 -0500

    removed right and back offset, added a true find path one piece to test accuracy

--- src/main/java/frc/robot/commands/TestAuto.java
+++ src/main/java/frc/robot/commands/TestAuto.java
@@ -12,7 +12,9 @@ import frc.robot.subsystems.LimeLightSubsystem;
 
 public class TestAuto extends SequentialCommandGroup {
     public TestAuto(DriveSubsystem m_driveSubsystem, LimeLightSubsystem m_lightSubsystem){
-        PathPlannerAuto auto = new PathPlannerAuto("3P Middle Top Bottom");
+        //PathPlannerAuto auto = new PathPlannerAuto("3P Middle Top Bottom");
+        PathPlannerAuto auto = new PathPlannerAuto("1P Middle Middle");
+
         addCommands(
             m_driveSubsystem.findPathToPose(auto.getStartingPose(), DriverStation.getAlliance().get() == Alliance.Red),
             auto
