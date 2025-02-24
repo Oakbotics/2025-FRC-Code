7c2d0ae746fe1ec7c060bc434c0202a55867cc7a
SuperCoderNikita <kisiliovnikita@gmail.com>
2025-02-23 19:59:42 -0500

    added L4 command group without intake

--- src/main/java/frc/robot/commands/L4ScoreCommandGroup.java
+++ src/main/java/frc/robot/commands/L4ScoreCommandGroup.java
@@ -1,16 +1,16 @@
-// package frc.robot.commands;
+package frc.robot.commands;
 
-// import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-// import frc.robot.subsystems.ArmSubsystem;
-// import frc.robot.subsystems.ElevatorSubsystem;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.WristSubsystem;
+import frc.robot.Constants.MotionConstants;
+import frc.robot.subsystems.ElevatorSubsystem;
 
-// public class L4ScoreCommandGroup extends ParallelCommandGroup {
-//     public L4ScoreCommandGroup(ElevatorSubsystem m_elevatorSubsystem, ArmSubsystem m_armSubsystem){
-//         addCommands(
-//             new ArmL4Command(m_armSubsystem),
-//             // wait(250),
-//             new ElevatorL4Command(m_elevatorSubsystem)
-//         );
-//     }
-// }
-
+public class L4ScoreCommandGroup extends ParallelCommandGroup {
+    public L4ScoreCommandGroup(ElevatorSubsystem m_elevatorSubsystem, WristSubsystem m_wristSubsystem){
+        addCommands(
+            new WristPositionCommand(m_wristSubsystem, 180.0).onlyIf(() -> (m_elevatorSubsystem.getElevatorHeight() < 0.01)),
+            new ElevatorPositionCommand(m_elevatorSubsystem, MotionConstants.l4.get("elevator").doubleValue()),
+            new WristPositionCommand(m_wristSubsystem, MotionConstants.l4.get("wrist").doubleValue())
+        );
+    }
+}
