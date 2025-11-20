b254219704bc019c04c469d3a9778e725fa01316
LukeTheCoder11 <tlqanimation@gmail.com>
2025-11-19 20:41:07 -0500

    ready to teach wrist to position command (YAYAYAY)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,12 +7,15 @@ package frc.robot;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.ElevatorWristCommands.CoralIntakeCommand;
 import frc.robot.commands.ElevatorWristCommands.CoralOuttakeCommand;
+import frc.robot.commands.ElevatorWristCommands.WristPositionCommand;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.WristSubsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
   CommandXboxController m_driverController = new CommandXboxController(OIConstants.kDriverControllerPort);
   private final IntakeSubsystem m_intakeSubsytem = new IntakeSubsystem();
+  private final WristSubsystem wrist = new WristSubsystem();
 
 
   public RobotContainer() {
@@ -27,6 +30,7 @@ public class RobotContainer {
 
     m_driverController.a().whileTrue(new CoralIntakeCommand(m_intakeSubsytem));
     m_driverController.b().whileTrue(new CoralOuttakeCommand(m_intakeSubsytem));
+    m_driverController.a().onTrue(new WristPositionCommand(wrist, 30.0));
   }
 
 
